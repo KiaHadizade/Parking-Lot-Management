c03fd2a5b8b67896e6e6c84154db92a517b3a0e1
Kia Hadizade <kiahadizade3077@gmail.com>
2025-01-21 04:36:01 +0330

    Added some functions for Admin

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import csv
-# import datetime
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -97,33 +96,35 @@ def fee(vehicle_name, start, end):
 
 def check_in():
     try:
-        with open(user_check_in, mode='r', newline='') as file:
-            read = csv.DictReader(file)
-            capacity = 0
-            for _ in read:
-                capacity += 1
-        if capacity < MAX_CAP:
-            with open(user_check_in, mode='a', newline='') as file:
-                writer = csv.writer(file)
-                headers = ['License plate number', 'vehicle', 'Brand Name', 'Owner Name', 'City', 'Check in Time']
-                file_op(user_check_in, headers)
+        with open(user_check_in, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            headers = ['License Plate', 'Vehicle', 'Brand Name', 'Owner Name', 'City', 'Check in Time']
+            file_op(user_check_in, headers)
 
+            with open(user_check_in, mode='r', newline='') as file:
+                read = csv.DictReader(file)
+                capacity = 0
+                for _ in read:
+                    capacity += 1
+            if capacity < MAX_CAP:
                 owner = input('Enter your full name: ')
-                License_plate = input('Enter your License plate number: ')
+                License_plate = input('Enter your License Plate: ')
                 vehicle = show_vehicle_brand()
                 brand_Name = input('Enter your vehicle model: ')
                 city = city_recognition(License_plate)
                 check_in_time = date()
                 writer.writerow([License_plate, vehicle, brand_Name, owner, city, check_in_time])
                 print('data saved successfully\n')
-        else:
-            print('Parking lot has reached the maximum capacity!\n')
+            else:
+                print('Parking lot has reached the maximum capacity!\n')
+            
+        
     except Exception as err:
         print(f'Error: {err}')
 
 def check_out():
     try:
-        license_plate_to_remove = input('Enter the License Plate Number to check out: ')
+        license_plate_to_remove = input('Enter the License Plate to check out: ')
         end_date = date()
         found_plate = False
         updated_rows = []
@@ -131,8 +132,8 @@ def check_out():
         with open(user_check_in, 'r', newline='') as read_file:
             check_in_records = csv.DictReader(read_file)
             for record in check_in_records:
-                if record['License plate number'] == license_plate_to_remove:
-                    vehicle_name = record['vehicle']
+                if record['License Plate'] == license_plate_to_remove:
+                    vehicle_name = record['Vehicle']
                     brand_name = record['Brand Name']
                     owner_name = record['Owner Name']
                     city = record['City']
@@ -150,7 +151,7 @@ def check_out():
             writer.writerows(updated_rows)
         
         print(f'User with License Plate {license_plate_to_remove} checked out.')
-        headers = ['License plate number', 'vehicle', 'Brand Name', 'Owner Name', 'City', 'Check in Time', 'Check out Time', 'Fee']
+        headers = ['License Plate', 'Vehicle', 'Brand Name', 'Owner Name', 'City', 'Check in Time', 'Check out Time', 'Fee']
         file_op(user_check_out, headers)
 
         with open(user_check_out, 'a', newline='') as check_out_file:
@@ -190,47 +191,137 @@ def generate_bill(license_plate, vehicle_name, brand_name, owner_name, city, che
 
 # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-def reporting():
+def view_info():
     df = pd.read_csv(user_check_in)
     print(f'{df}\n')
-    car = motorcycle = bicycle = truck = bus = van = luxury_vehicle = electric_car = scooter = 0
-    for x in df.index:
-        if df.loc[x, 'vehicle'] == 'Car': car += 1
-        elif df.loc[x, 'vehicle'] == 'Motorcycle': motorcycle += 1
-        elif df.loc[x, 'vehicle'] == 'Bicycle': bicycle += 1
-        elif df.loc[x, 'vehicle'] == 'Truck': truck += 1
-        elif df.loc[x, 'vehicle'] == 'Bus': bus += 1
-        elif df.loc[x, 'vehicle'] == 'Van': van += 1
-        elif df.loc[x, 'vehicle'] == 'Luxury Vehicle': luxury_vehicle += 1
-        elif df.loc[x, 'vehicle'] == 'Electric Car': electric_car += 1
-        else: scooter += 1
-    print(f'Car: {car}', end='')
-    print(f', Motorcycle: {motorcycle}', end='')
-    print(f', Bicycle: {bicycle}', end='')
-    print(f', Truck: {truck}', end='')
-    print(f', Bus: {bus}', end='')
-    print(f', Van: {van}', end='')
-    print(f', Luxury Vehicle: {luxury_vehicle}', end='')
-    print(f', Electric Car: {electric_car}')
-    print('')
 
-def runMatch():
+def revenue_report():
+    df = pd.read_csv(user_check_out)
+
+    # Convert 'Check out Time' to datetime
+    df['Check out Time'] = pd.to_datetime(df['Check out Time'])
+    
+    # Get today's date and the current week, month and year
+    today = datetime.today()
+    start_of_today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+    start_of_week = start_of_today - timedelta(days=today.weekday()) # Start of current week is Monday
+    start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    start_of_year = today.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+    
+    # Filters for each time period, values are boolean
+    filters = {
+        "Today": (df['Check out Time'] >= start_of_today),
+        "This Week": (df['Check out Time'] >= start_of_week),
+        "This Month": (df['Check out Time'] >= start_of_month),
+        "This Year": (df['Check out Time'] >= start_of_year),
+    }
+    
+    # revenue for each period
+    report = {period: df.loc[filter]['Fee'].sum() for period, filter in filters.items()}
+    # Add the total revenue to the report
+    report['Total Revenue'] = df['Fee'].sum()
+    print(f'''
+    Today: ${report['Today']:.2f}
+    This Week: ${report['This Week']:.2f}
+    This Month: ${report['This Month']:.2f}
+    This Year: ${report['This Year']:.2f}
+    Total Income: ${report['Total Revenue']:.2f}
+    ''')
+
+def reporting():
+    # Display of vehicles and their count based on the type of vehicle
+    df = pd.read_csv(user_check_in)
+    print('Display of Parking Lot:\n', df)
+
+    type_count = df.groupby('Vehicle').size().reset_index(name='Number of Vehicles')
+    print('\nCount of any parked vehicle\n', type_count)
+
+    # Breakdown based on today's vehicles and the last 7 days
+    df['Check in Time'] = pd.to_datetime(df['Check in Time'])
+    
+    today = datetime.today()
+    start_of_today = today.replace(hour=0, minute=0, second=0, microsecond=0)
+    start_of_last_7_days = start_of_today - timedelta(days=7)
+    
+    filters = {
+        "Today": (df['Check in Time'] >= start_of_today),
+        "Last 7 Days": (df['Check in Time'] >= start_of_last_7_days)
+    }
+    
+    reports = {period: df.loc[filter] for period, filter in filters.items()}
+    for period, report in reports.items():
+        print(f'\n{period}:\n', report)
+
+    # Providing vehicle statistics based on the city of license plate registration (how many vehicles are from each city)
+    city_count = df.groupby('City').size().reset_index(name='Number of Vehicles')
+    print('\nCount of vehicle based on the city\n', city_count)
+
+    # Providing statistics on vehicles with even and odd license plate
+    even = df[df['License Plate'].apply(lambda x: int(x[-1]) % 2 == 0)]
+    odd = df[df['License Plate'].apply(lambda x: int(x[-1]) % 2 != 0)]
+    print(f'\nEven License Plate: \n{even}')
+    print(f'\nOdd License Plate: \n{odd}\n')
+
+def search():
+    df = pd.read_csv(user_check_in)
+    search = input('Enter a License Plate or vehicle type to search: ')
+    for x in df.index:
+        if not (df.loc[x, 'License Plate'] == search or df.loc[x, 'Vehicle'] == search):
+            df.drop(x, inplace= True)
+    if df.empty:
+        print('Nothing founded!')
+    else:
+        print(f'\n{df}\n')
+
+def RunMenu():
+    # Main Menu
     while True:
         try:
-            num = int(input('0. To exit\n1. Enter as Admin\n2. Enter as user\n'))
+            num = int(input('0. To exit\n1. Enter as Admin\n2. Enter as User\n'))
             
             match num:
                 case 0:
                     break
                 case 1:
-                    # Admin section
-                    adm_pws = input("Enter Admin password: ")
+                    # Admin Menu
+                    adm_pws = input("Enter Password: ")
                     if adm_pws == ADMIN_PSW:
-                        reporting()
+                        while True:
+                            try:
+                                adm_num = int(input('1. View Vehicles and Their Info\n2. Search Vehicle\n3. Reporting Info\nPress 0 to Exit\n'))
+                                
+                                match adm_num:
+                                    case 0:
+                                        print("Exiting Admin Menu.")
+                                        break
+                                    case 1:
+                                        view_info()
+                                    case 2:
+                                        search()
+                                    case 3:
+                                        # Reporting
+                                        try:
+                                            Rpt_num = int(input('1. Parking Revenue Report\n2. Report On The Number Of Parked Vehicles\nPress 0 to Exit\n'))
+                                            
+                                            match Rpt_num:
+                                                case 0:
+                                                    break
+                                                case 1:
+                                                    revenue_report()
+                                                case 2:
+                                                    reporting()
+                                                case _:
+                                                    print("Invalid input. Please enter 1, 2 or 0.")
+                                        except ValueError:
+                                            print("Invalid input. Please enter a valid number.")
+                                    case _:
+                                        print("Invalid input. Please enter 1, 2, 3 or 0.")
+                            except ValueError:
+                                print("Invalid input. Please enter a valid number.")
                     else:
                         print("Incorrect password!")
                 case 2:
-                    # User section
+                    # User Menu
                     print('Welcome to Parking Lot!\n')
                     while True:
                         try:
@@ -238,7 +329,7 @@ def runMatch():
                             
                             match user_num:
                                 case 0:
-                                    print("Exiting user section.")
+                                    print("Exiting User Menu.")
                                     break
                                 case 1:
                                     check_in()
@@ -253,6 +344,6 @@ def runMatch():
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-# runMatch()
+# RunMenu()
 if __name__ == "__main__":
-    runMatch()
+    RunMenu()
